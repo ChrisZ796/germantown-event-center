ab5e32d5fdc839f0fff2fffe4f8146670b6d2160
ChrisZ-360 <chrisz.wshs@gmail.com>
2026-03-21 09:34:03 -0500

    feat sign up page

--- src/contexts/Authentication.tsx
+++ src/contexts/Authentication.tsx
@@ -4,14 +4,23 @@ export interface AuthenticationContextType {
     setUsername: (username: string) => void;
     password: string;
     setPassword: (password: string) => void;
+    firstname: string;
+    setFirstname: (firstname: string) => void;
+    lastname: string;
+    setLastname: (lastname: string) => void;
+    email: string;
+    setEmail: (email: string) => void;
 }
 export const AuthenticationContext = createContext<AuthenticationContextType>({} as AuthenticationContextType);
 
 export function AuthenticationProvider({ children }: { children: React.ReactNode }) {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [firstname, setFirstname] = useState('');
+    const [lastname, setLastname] = useState('');
+    const [email, setEmail] = useState('');
     return (
-        <AuthenticationContext.Provider value={{ username, setUsername, password, setPassword }}>
+        <AuthenticationContext.Provider value={{ username, setUsername, password, setPassword, firstname, setFirstname, lastname, setLastname, email, setEmail }}>
             {children}
         </AuthenticationContext.Provider>
     );
